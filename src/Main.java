2e51891cf4d6609d056f6a02a71c304b73d374e7
peterk0000 <kpa0400@yandex.ru>
2026-01-18 22:30:30 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class Main {
+    private static int quene = 1;
+    private static Object lock = new Object();
+    private static int totalThreads;
+
+    public static void main(String[] args) throws InterruptedException {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Введите число потоков: ");
+        totalThreads = sc.nextInt();
+        sc.close();
+
+        for (int i = 1; i <=totalThreads; i++) {
+
+            final int numberOfThread = i;
+
+            Thread thread = new Thread("Поток-" + i) {
+              @Override
+              public void run() {
+                  try {
+                      for (int i = 1; i < 100; i++) {
+                          synchronized (lock) {
+                              while (quene != numberOfThread) {
+                                  lock.wait();
+
+                              }
+
+                              System.out.println(getName());
+
+                              if (numberOfThread == totalThreads) {
+                                  quene = 1;
+                              } else {
+                                  quene = numberOfThread + 1;
+                              }
+
+                              lock.notifyAll();
+                          }
+
+                      }
+                  } catch (InterruptedException e) {
+                      e.printStackTrace();
+                  }
+              }
+            };
+
+            thread.start();
+
+        }
+    }
+}
